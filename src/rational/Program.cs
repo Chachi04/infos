b6238410c542c402e04ac57f9c06584ed86bd817
Jiaqi Wang <w.jiaqi004@gmail.com>
2021-11-29 16:30:04 +0200

    csharp documentations

--- src/rational/Program.cs
+++ src/rational/Program.cs
@@ -2,6 +2,10 @@
 
 namespace rational
 {
+    /// <summary>
+    /// Gives the ability to use rational numbers, represented
+    /// by a numerator and a denumerator, different from zero.
+    /// <summary>
     class Rational
     {
         private int _num;
@@ -9,8 +13,17 @@ namespace rational
 
         public double Dec => (double)this._num / this._den;
 
+        /// <summary>
+        /// Instanciates the class
+        /// </summary>
         public Rational() { }
 
+        /// <summary>
+        /// Instanciates the class with given atributes
+        /// </summary>
+        /// <param name="num">Numberator</param>
+        /// <param name="den">Denumerator</param>
+        /// <exception cref="ArgumentException">Denominator cannot be zero</exception>
         public Rational(int num, int den)
         {
             if (den == 0)
@@ -21,18 +34,33 @@ namespace rational
             this._den = den;
         }
 
+        /// <summary>
+        /// Instanciates a class to the same as another instance of the class
+        /// </summary>
+        /// <param name="r">A rational that is being copied</param>
         public Rational(Rational r)
         {
             this._num = r._num;
             this._den = r._den;
         }
 
+        /// <summary>
+        /// Parses a string of the type int/int to a rational
+        /// </summary>
+        /// <param name="s">The string that is being parsed</param>
+        /// <returns>The parsed Rational</returns>
         public static Rational Parse(string s)
         {
             string[] str = s.Split('/');
             return new Rational(int.Parse(str[0]), int.Parse(str[1]));
         }
 
+        /// <summary>
+        /// Tries to parse a string to rational.
+        /// </summary>
+        /// <param name="s">String that is parsed</param>
+        /// <param name="r">The rational that is outed</param>
+        /// <returns>true if is parsed successfully and false if it wasn't</returns>
         public static bool TryParse(string s, out Rational r)
         {
             string[] str = s.Split('/');
@@ -55,34 +83,87 @@ namespace rational
             return true;
         }
 
-
         public static Rational operator +(Rational r) => r;
         public static Rational operator -(Rational r) => new Rational(-r._num, r._den);
 
+        /// <summary>
+        /// Adds two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the sum of r1 and r2</returns>
         public static Rational operator +(Rational r1, Rational r2)
             => new Rational(r1._num * r2._den + r2._num * r1._den, r1._den * r2._den);
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static Rational operator -(Rational r1, Rational r2)
             => new Rational(r1._num * r2._den - r2._num * r1._den, r1._den * r2._den);
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static Rational operator *(Rational r1, Rational r2)
             => new Rational(r1._num * r2._num, r1._den * r2._den);
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static Rational operator /(Rational r1, Rational r2)
             => new Rational(r1._num * r2._den, r1._den * r2._num);
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static bool operator ==(Rational r1, Rational r2)
             => r1._num * r2._den == r2._num * r1._den;
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static bool operator !=(Rational r1, Rational r2)
             => r1._num * r2._den != r2._num * r1._den;
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static bool operator >=(Rational r1, Rational r2)
             => r1._num * r2._den >= r2._num * r1._den;
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public static bool operator <=(Rational r1, Rational r2)
             => r1._num * r2._den <= r2._num * r1._den;
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         private void Simp()
         {
             int gcd = GCD(this._num, this._den);
@@ -90,6 +171,12 @@ namespace rational
             this._den /= gcd;
         }
 
+        /// <summary>
+        /// Calculates the greated common divisor of two integers
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>Greatest common divisor</returns>
         private int GCD(int a, int b)
         {
             if (b != 0)
@@ -97,6 +184,12 @@ namespace rational
             return a;
         }
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public override string ToString()
         {
             this.Simp();
@@ -108,6 +201,12 @@ namespace rational
 
         public override int GetHashCode() => 0;
 
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r1">First rational number</param>
+        /// <param name="r2">Second rational number</param>
+        /// <returns>A rational number that is the difference of r1 and r2</returns>
         public double ToDouble() => (double)_num / _den;
 
     }
--- src/rational0.1/Program.cs
+++ src/rational0.1/Program.cs
@@ -2,29 +2,46 @@
 
 namespace rational0._1
 {
+    /// <summary>
+    /// Gives the ability to use rational numbers, represented
+    /// by a numerator and a denumerator, different from zero.
+    /// <summary>
     class Rational
     {
         public int num;
         public int den;
 
-        public Rational()
-        {
-            this.num = 0;
-            this.den = 1;
-        }
+        /// <summary>
+        /// Instanciates the class
+        /// </summary>
+        public Rational() { }
 
+        /// <summary>
+        /// Instanciates the class with given atributes
+        /// </summary>
+        /// <param name="num">Numberator</param>
+        /// <param name="den">Denumerator</param>
+        /// <exception cref="ArgumentException">Denominator cannot be zero</exception>
         public Rational(int num, int den)
         {
             this.num = num;
             this.den = den;
         }
 
+        /// <summary>
+        /// Instanciates a class to the same as another instance of the class
+        /// </summary>
+        /// <param name="r">A rational that is being copied</param>
         public Rational(Rational r)
         {
             this.num = r.num;
             this.den = r.den;
         }
 
+        /// <summary>
+        /// Adds a rational to this one
+        /// </summary>
+        /// <param name="r">The ratioanl that is being add</param>
         public void Add(Rational r)
         {
             this.num = this.num * r.den + r.num * this.den;
@@ -32,6 +49,11 @@ namespace rational0._1
             this.Simp();
         }
 
+
+        /// <summary>
+        /// Subtracts two rational numbers
+        /// </summary>
+        /// <param name="r">The second rational</param>
         public void Sub(Rational r)
         {
             this.num = this.num * r.den - r.num * this.den;
@@ -39,6 +61,10 @@ namespace rational0._1
             this.Simp();
         }
 
+        /// <summary>
+        /// Multiplies two rational numbers
+        /// </summary>
+        /// <param name="r">The second rational</param>
         public void Mult(Rational r)
         {
             this.num = this.num * r.num;
@@ -46,6 +72,10 @@ namespace rational0._1
             this.Simp();
         }
 
+        /// <summary>
+        /// Devides two rational numbers
+        /// </summary>
+        /// <param name="r">The second rational</param>
         public void Dev(Rational r)
         {
             this.num = this.num * r.den;
@@ -53,6 +83,14 @@ namespace rational0._1
             this.Simp();
         }
 
+        /// <summary>
+        /// Compares the second rational to the first.
+        /// Returns 1 if the first is greater,
+        /// Returns -1 if the second is greated,
+        /// Returns 0 if they are the same.
+        /// </summary>
+        /// <param name="r">The second rational</param>
+        /// <returns>1,-1 or 0</returns>
         public int Compare(Rational r)
         {
             if (this.num * r.den > r.num * this.den) return 1;
@@ -60,11 +98,17 @@ namespace rational0._1
             return 0;
         }
 
+        /// <summary>
+        /// Print the rational in decimal form
+        /// </summary>
         public void PrintDec()
         {
             Console.WriteLine(Math.Round((double)this.num / this.den, 2));
         }
 
+        /// <summary>
+        /// Print the rational in normal form
+        /// </summary>
         public void Print()
         {
             if (this.den == 1 || this.num == 0)
@@ -73,6 +117,10 @@ namespace rational0._1
                 Console.WriteLine($"{this.num} / {this.den}");
         }
 
+        /// <summary>
+        /// Multiply two rational numbers
+        /// </summary>
+        /// <param name="r">The second rational</param>
         private void Simp()
         {
             int d = this.GCD(this.num, this.den);
@@ -80,6 +128,12 @@ namespace rational0._1
             this.den /= d;
         }
 
+        /// <summary>
+        /// Calculates the greated common divisor of two integers
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>Greatest common divisor</returns>
         private int GCD(int a, int b) => b != 0 ? GCD(b, a % b) : a;
     }
 
