c94629a7a055950c622812c03341d27c048c0cfd
Chachi-tech <wang214649@gmail.com>
2021-12-17 13:40:32 +0200

    17.12.2021

---
+++ src/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exceptions
+{
+    public class MyException : Exception
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "hello.txt";
+            try
+            {
+                
+                using (StreamReader r = new StreamReader(filename))
+                {
+                    int[] a = new int[100];
+                    int cnt;
+                    cnt = int.Parse(r.ReadLine());
+                    for(int i = 0; i<= cnt; i++)
+                    {
+                        a[i] = int.Parse(r.ReadLine());
+                    }
+                }
+            }
+            catch (FileNotFoundException notfound)
+            {
+                Console.WriteLine($"No such file: {filename}\nException {notfound.Message}");
+            }
+            catch (FormatException notint)
+            {
+                Console.WriteLine($"Number is not\nException {notint.Message}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Unexpected end\nException {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/ITriangle/ITriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITriangle
+{
+    interface ITriangle
+    {
+        double Perimeter();
+        double Area();
+    }
+    class Triangle : ITriangle
+    {
+        private double a,b,c;
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double Perimeter()
+        {
+            return a + b + c;
+        }
+        public double Area()
+        {
+            double p = this.Perimeter() / 2;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t = new Triangle(3, 4, 5);
+            Console.WriteLine(t.Perimeter());
+            Console.WriteLine(t.Area());
+        }
+    }
+}
