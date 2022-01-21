f720afbf01f450fc43419bae7aa112790af6ee30
Jiaqi Wang <w.jiaqi004@gmail.com>
2022-01-21 13:30:45 +0200

    recursion 21.01.2022

---
+++ src/recursive-factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace recursive_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a num: ");
+                Console.WriteLine(factorial(int.Parse(Console.ReadLine())));
+            }
+        }
+
+        static long factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * factorial(n - 1);
+        }
+    }
+}
---
+++ src/recursive-fibonachi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace recursive_fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a num: ");
+                Console.WriteLine(fib(int.Parse(Console.ReadLine())));
+            }
+        }
+        static long fib(int n)
+        {
+            if (n <= 0) return fib(n + 2) - fib(n + 1);
+            if (n == 1 || n == 2) return 1;
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
