3903521904c8f0523d7c7e31f25f1a352a318b76
Jiaqi Wang <w.jiaqi004@gmail.com>
2022-02-09 11:54:56 +0200

    recursion page 25 ex1

---
+++ src/recursion-ex1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace recursion_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, n;
+
+            Console.WriteLine("Recursion page 25");
+            Console.WriteLine("ex1 a");
+            Console.Write("Enter x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(a(x, n));
+
+            Console.WriteLine("ex1 b");
+            Console.Write("Enter x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(b(x, n));
+
+            Console.WriteLine("ex1 c");
+            Console.Write("Enter n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(c(n));
+        }
+
+        static double a(int x, int n)
+        {
+            if (n == 0) return 1;
+            return Math.Pow(x, n) + a(x, n - 1);
+        }
+
+        static double b(int x, int n)
+        {
+            if (n == 0) return 1;
+            return b(x, n - 1) + (1 / Math.Pow(x, n));
+        }
+
+        static double c(int n)
+        {
+            if (n == 1) return n * (n + 1);
+            return c(n - 1) + n * (n + 1);
+        }
+
+    }
+}
---
+++ src/recursion-ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace recursion_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
